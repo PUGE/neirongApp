ebf997350adf33c3c1e4485fdf15ffab233bc7cf
puge <mail@puge.org>
2020-11-18 00:27:59 +0800

    save

--- insert/preload.js
+++ insert/preload.js
@@ -8,37 +8,8 @@ const serverIP = 'http://49.232.216.171:8006/'
 // const serverIP = 'http://127.0.0.1:8006/'
 let findList = {}
 let findListArr = []
-function getErrorTypeText (errorCode) {
-  switch (errorCode) {
-    case 'Polity':
-      return '政治'
-      break;
-    case 'Illegal':
-      return '违法'
-      break;
-    case 'Porn':
-      return '色情'
-      break;
-    case 'Abuse':
-      return '粗俗'
-      break;
-    case 'Terror':
-      return '恐怖'
-      break;
-    case 'Standard':
-      return '错误表述'
-      break;
-    case 'Ad':
-      return '广告'
-      break;
-    case 'XiYu':
-      return '/重要句子'
-      break;
-    default:
-      return errorCode
-      break;
-  }
-}
+let rightNum = 0
+let errorNum = 0
 
 function chengyuBaseHandle (htmlData, data) {
   data.forEach(item => {
@@ -49,6 +20,7 @@ function chengyuBaseHandle (htmlData, data) {
       item.tips = `<h2 style="font-size: 20px;">${item['text']}</h2><h2 style="font-size: 20px;">[${item['pinyin2']}]</h2><p>释义：${item['interpretation']}</p><p>出处：${item['source']}</p><p>示例：${item['example']}</p>`
       // 如果词的类型为政治词语，不一致为错误，其他类型不一致为对应类型
       htmlData = htmlData.replace(new RegExp(item['text'], "gm"), `<span data-ind="${findListArr.length}" class="nrsh chengyu chengyu-base">${item['text']}</span>`)
+      rightNum++
     }
   })
   return htmlData
@@ -65,6 +37,7 @@ function chengyuPinyinHandle (htmlData, data) {
       item.tips = `<h2 style="font-size: 20px;">${item['text']}</h2><h2 style="font-size: 20px;">[${item['pinyin2']}]</h2><p>释义：${item['interpretation']}</p><p>出处：${item['source']}</p><p>示例：${item['example']}</p>`
       // 如果词的类型为政治词语，不一致为错误，其他类型不一致为对应类型
       htmlData = htmlData.replace(new RegExp(item['like'], "gm"), `<span data-ind="${findListArr.length}" class="nrsh chengyu chengyu-like">${item['like']}</span>`)
+      errorNum++
     }
   })
   return htmlData
@@ -81,6 +54,7 @@ function baseHandle (htmlData, data) {
       } else {
         htmlData = htmlData.replace(new RegExp(item['text'],"gm"), `<span data-ind="${findListArr.length}" class="nrsh base ${item.type}">${item['text']}</span>`)
       }
+      rightNum++
     }
   })
   return htmlData
@@ -98,6 +72,7 @@ function pinyinHandle (htmlData, data) {
       } else {
         htmlData = htmlData.replace(new RegExp(item['like'], "gm"), `<span data-ind="${findListArr.length}" class="nrsh like ${item.type}">${item['like']}</span>`)
       }
+      errorNum++
     }
   })
   return htmlData
@@ -128,6 +103,7 @@ function regexpHandle (htmlData, data) {
       } else {
         htmlData = htmlData.replace(new RegExp(item['like'], "gm"), `<span data-ind="${findListArr.length}" class="nrsh regexp XiYu">${item['like']}</span>`)
       }
+      errorNum++
     }
   })
   return htmlData
@@ -168,12 +144,14 @@ window.addEventListener('DOMContentLoaded', () => {
   
   var insertElement = document.createElement("div");
   insertElement.classList.add('neirong-box')
-  insertElement.innerHTML = `<div class="neirong-top"><input type="text" class="neirong-text" value="http://www.people.com.cn/" placeholder="输入网址链接"><div class="neirong-button neirong-button-1">转到</div><div class="neirong-button neirong-button-2">检查</div></div><div class="neirong-bottom"><input id="neirongAutoReload" type="checkbox" name="autoReload" value="自动刷新"/><span>每隔</span><input type="number" value="60"><span>秒自动刷新页面</span>  <input id="neirongAlert" type="checkbox" name="autoReload" value=""/><span>网页内容变更提醒</span><input id="neirongErrorAlert" type="checkbox" name="autoReload" value=""/><span>疑似错误提醒</span></div>`
+  insertElement.innerHTML = `<div class="neirong-top"><input type="text" class="neirong-text" value="http://www.people.com.cn/" placeholder="输入网址链接"><div class="neirong-button neirong-button-1">转到</div><div class="neirong-button neirong-button-2">检查</div></div><div class="neirong-bottom"><input id="neirongAutoReload" type="checkbox" name="autoReload" value="自动刷新"/><span>每隔</span><input type="number" value="60"><span>秒自动刷新页面</span>  <input id="neirongAlert" type="checkbox" name="autoReload" value=""/><span>网页内容变更提醒</span><input id="neirongErrorAlert" type="checkbox" name="autoReload" value=""/><span>疑似错误提醒</span><div class="neirong-number-info"></div></div>`
   document.body.appendChild(insertElement);
   var insertElement2 = document.createElement("div");
   insertElement2.classList.add('neirong-show')
   document.body.appendChild(insertElement2);
   function checkConn () {
+    rightNum = 0
+    errorNum = 0
     let TextArr = []
     document.querySelectorAll('a').forEach(element => {
       TextArr.push(element.innerText)
@@ -241,6 +219,11 @@ window.addEventListener('DOMContentLoaded', () => {
       const neirongShow = document.querySelector('.neirong-show')
       neirongShow.style.display = 'block'
       setTimeout(() => {
+        document.querySelector('.neirong-number-info').innerText = `正确关键词：${rightNum}个 疑似错误：${errorNum}个`
+        // 如果有错误报警
+        if (errorNum > 0) {
+          new Audio("https://cunchu.site/app/neirong/ding.mp3").play()
+        }
         // 清理嵌套
         document.querySelectorAll('.nrsh .nrsh').forEach(element => {
           element.outerHTML = element.innerText
@@ -253,12 +236,12 @@ window.addEventListener('DOMContentLoaded', () => {
               if (!data) return
               let newHtml = '<div class="neirong-title">详细信息</div>'
               if (data.like) {
-                newHtml += `${data.like}<br>正确的文字可能为: ${data.text}`
+                newHtml += `${data.like}<br>正确的文字可能为: ${data.text}<br>`
               } else {
                 newHtml += `${data.text}<br>正确关键词！`
               }
               if (data.likeNumber) {
-                newHtml += `相似度: ${data.likeNumber}%<br>`
+                newHtml += `相似度: ${data.likeNumber}%`
               }
               if (data.tips) {
                 newHtml += `<div class="neirong-info">${data.tips}</div>`
